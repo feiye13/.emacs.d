47c190a7b4e9ff35d4a9135f3bb534b2dcc5c6b0
miire <miire>
2022-08-02 09:34:08 +0800

    Update config

--- lisp/config/init-key.el
+++ lisp/config/init-key.el
@@ -3,13 +3,13 @@
 
 ;; meow
 (meow-leader-define-key
- '(";" . meow-keypad-start))
+ '("," . meow-keypad-start))
 
 ;; recentf
 (global-set-key (kbd "C-x C-r") 'recentf-open-files)
 
 
 ;; snails
-(global-set-key (kbd "C-; C-s") 'snails)
+(global-set-key (kbd "C-, C-s") 'snails)
 
 (provide 'init-key)

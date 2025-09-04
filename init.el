6d054a8facfd7052caab13e86d851eb2e2093a4b
feiye13 <none>
2025-09-04 16:33:58 +0800

    Add files

---
+++ init.el
@@ -0,0 +1,16 @@
+;;; init.el --- emacs config -*- lexical-binding: t -*-
+;;; Commentary:
+
+;;; Code:
+
+(add-to-list 'load-path "~/.emacs.d/lisp/")
+
+(setq custom-file (locate-user-emacs-file "custom.el"))
+(when (file-exists-p custom-file)
+    (load custom-file))
+
+(require 'init-base)
+(require 'init-ui)
+
+(provide 'init)
+;;; init.el ends here
---
+++ lisp/init-ui.el
@@ -0,0 +1,26 @@
+;;; init-ui.el --- ui -*- lexical-binding: t -*-
+;;; Commentary:
+
+;;; Code:
+
+;; Cursor
+(blink-cursor-mode -1) ;; 关闭光标闪动
+(setq-default cursor-type '(bar . 2))
+
+(add-hook 'after-init-hook 'global-display-line-numbers-mode) ;; 显示行号
+(add-hook 'after-init-hook 'global-hl-line-mode) ;; 高亮光标所在行
+
+
+;; Font
+(when window-system
+    (set-frame-font (font-spec :family "LXGW WenKai Mono Medium" :size 22))
+    (dolist (script '(han cjk-misc bopomofo))
+        (set-fontset-font 
+            (frame-parameter nil 'font) 
+            script
+            (font-spec :name "LXGW WenKai Mono Medium" :size 22))))
+
+(load-theme 'modus-operandi)
+
+(provide 'init-ui)
+;;; init-ui.el ends here

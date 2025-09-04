90dd514296cf44a2d0816d3bc0843301dc5b89cc
feiye13 <none>
2025-09-04 18:02:19 +0800

    Update 'lisp/init-package.el'

--- lisp/init-package.el
+++ lisp/init-package.el
@@ -3,7 +3,13 @@
 
 ;;; Code:
 
+(require 'package)
+(require 'use-package)
+
 (package-initialize)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
 
 (setq use-package-always-ensure t)
 

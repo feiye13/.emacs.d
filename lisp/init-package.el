0aa6af888beb6ebd703f3b3e082a57bcb903eb36
feiye13 <none>
2025-09-04 20:41:33 +0800

    Switch package mirror

--- lisp/init-package.el
+++ lisp/init-package.el
@@ -6,6 +6,10 @@
 (require 'package)
 (require 'use-package)
 
+(setq package-archives '(("gnu" . "https://mirrors.ustc.edu.cn/elpa/gnu/")
+                         ("melpa" . "https://mirrors.ustc.edu.cn/elpa/melpa/")
+                         ("nongnu" . "https://mirrors.ustc.edu.cn/elpa/nongnu/")))
+
 (package-initialize)
 
 (when (not package-archive-contents)
